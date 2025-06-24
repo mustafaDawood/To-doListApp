3f59c049298f62243f2e7c1293fb52979a97b313
mustafa dawood <mustafa.dawood93@icloud.com>
2025-06-25 00:30:27 +0300

    Initial Commit

---
+++ To-doListApp/To_doListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_doListAppApp.swift
+//  To-doListApp
+//
+//  Created by Mustafa Dawood on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct To_doListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
