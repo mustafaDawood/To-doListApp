3fae1a31a32440d4b9eecec371c2ab1f4685b119
mustafa dawood <mustafa.dawood93@icloud.com>
2025-07-07 00:39:19 +0300

    Creating the view model + move and delete features

---
+++ To-doListApp/Model/ItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemModel.swift
+//  To-doListApp
+//
+//  Created by Mustafa Dawood on 05/07/2025.
+//
+
+import Foundation
---
+++ To-doListApp/ViewModel/ListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListViewModel.swift
+//  To-doListApp
+//
+//  Created by Mustafa Dawood on 07/07/2025.
+//
+
+import Foundation
